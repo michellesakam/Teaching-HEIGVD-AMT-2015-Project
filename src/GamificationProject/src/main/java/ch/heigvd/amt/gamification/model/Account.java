41669d46e038af50af482f21fe66509977be3e02
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2015-09-25 16:41:07 +0200

    Add first properties and constructors to Account model class

--- src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/model/Account.java
+++ src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/model/Account.java
@@ -5,5 +5,40 @@ package ch.heigvd.amt.gamification.model;
  * @author Olivier Liechti
  */
 public class Account {
+  
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
+
+  public Account() {
+  }
+
+  public Account(String firstName, String lastName, String email, String password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+  
+  
+  
+  
 
 }
