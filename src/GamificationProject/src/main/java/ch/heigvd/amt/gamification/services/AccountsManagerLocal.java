ab1ba34b20df8abc32ad396fc9e01999c2efebf5
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2015-09-30 06:17:09 +0200

    Update AccountsManagerLocal.java

--- src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/services/AccountsManagerLocal.java
+++ src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/services/AccountsManagerLocal.java
@@ -18,4 +18,15 @@ public interface AccountsManagerLocal {
     public void editAccount(String email, String newEmail, String newPassword, String newFirstName, String newLastName);
     
     // TODO: Demander au prof pour le paramètre e-mail
+    
+    /**
+     * email est un identificant unique pour l'Account, c'est donc tout à fait possible de l'utiliser comme paramètre
+     * pour la méthode getAccountApps. Une alternative serait de donnée l'id (le champs annoté avec @Id dans Account),
+     * mais ça serait probablement moins pratique (ça dépend de ce qui est codé dans le serlvet.
+     * 
+     * Sinon:
+     * - getNumberOfAccounts() est un peu mieux que numberOfAccount() (méthod = verb et account au pluriel)
+     * - pour create account et edit account, au lieu de passer chaque attribut en paramètre, vous pouvez passer
+     *   une instance de Account: createAccount(Account newAccount), updateAccount(Account accountToUpdate).
+     */
 }
