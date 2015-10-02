fc87124528297f7598ae8f95fade3ab986ebb0d5
unknown <samira.kouchali@heig-vd.ch>
2015-10-02 11:49:22 +0200

    ProjectTest

---
+++ GamificationProjectTest/src/test/java/ch/heigvd/amt/uat/selenium/pages/AbstractGamificationProject.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.amt.uat.selenium.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ *
+ * @author Samira
+ */
+public abstract class AbstractGamificationProject extends Page{
+
+    public AbstractGamificationProject(WebDriver driver) {
+        super(driver);
+    }
+    
+}
