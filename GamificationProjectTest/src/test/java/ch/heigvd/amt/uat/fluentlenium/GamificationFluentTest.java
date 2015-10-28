3d022e3a7ae112abeeb81e2d482075619105633d
unknown <samira.kouchali@heig-vd.ch>
2015-10-28 16:59:03 +0100

    Mise à jour tests

--- GamificationProjectTest/src/test/java/ch/heigvd/amt/uat/fluentlenium/GamificationFluentTest.java
+++ GamificationProjectTest/src/test/java/ch/heigvd/amt/uat/fluentlenium/GamificationFluentTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.fluentlenium.core.annotation.Page;
+import org.junit.After;
 
 /**
  *
@@ -17,6 +18,17 @@ import org.fluentlenium.core.annotation.Page;
 public class GamificationFluentTest extends FluentTest {
 
     private final String baseUrl = "http://localhost:8080/GamificationProject/";
+
+    private static int newEmailId = 0;
+
+    private void simulateLogin() {
+        goTo(baseUrl);
+        loginPage.isAt();
+        loginPage.typeEmailAddress("raphael.racine@heig-vd.ch");
+        loginPage.typePassword("toor");
+        loginPage.clickLogin();
+        yourAppPage.isAt();
+    }
 
     @Page
     public LoginFluentPage loginPage;
@@ -29,100 +41,92 @@ public class GamificationFluentTest extends FluentTest {
 
     @Page
     public ApplicationRegistrationFluentPage ApplicationRegistrationPage;
-    
-    
-     @Test
-     public void itShouldNotBePossibleToSigninWithAnInvalidEmail() {
-     goTo(baseUrl);
-     loginPage.isAt();
-     loginPage.typeEmailAddress("not a valid email");
-     loginPage.typePassword("any password");
-     loginPage.clickLogin();
-     loginPage.isAt();
-     }
-    
-   
 
-     @Test
-     public void itShouldBeNotPossibleToAccessSecurePagesWithoutLogin() {
-     goTo(baseUrl + "/pages/yourApps");
-     loginPage.isAt();
-     }
-    
-     @Test
-     public void itShouldBePossibleToAccessRegistrationPagesWithCreateAccountButton() {
-     goTo(baseUrl);
-     loginPage.clickCreateAccount();
-     RegistrationPage.isAt();
-     }
-    
-    
-     @Test
-     public void itShouldNotBePossibleToSignUpWithAnInvalidEmail() {
-     goTo(baseUrl + "pages/accountRegistration");
-     RegistrationPage.isAt();
-       
-     RegistrationPage.typeEmailAddress("not a valid email");
-     RegistrationPage.typeFirstname("FirstName");
-     RegistrationPage.typeLastname("LastName");
-     RegistrationPage.typePassword("any password");
-     RegistrationPage.typeConfirmPassword("any password");
-     RegistrationPage.clickSignUp();
-     RegistrationPage.isAt();
-        
-     }
-    
-    
-     @Test
-     public void itShouldBePossibleToAccessYoutAppsPagesWithCreateAnAccount() {
-       
-     goTo(baseUrl + "pages/accountRegistration");
-     RegistrationPage.isAt();
-       
-     RegistrationPage.typeEmailAddress("sara.kouch@gmail.ch");
-     RegistrationPage.typeFirstname("Sara");
-     RegistrationPage.typeLastname("Kouch");
-     RegistrationPage.typePassword("1234");
-     RegistrationPage.typeConfirmPassword("1234");
-     RegistrationPage.clickSignUp();
-     yourAppPage.isAt();
+    @Test
+    public void itShouldNotBePossibleToSigninWithAnInvalidEmail() {
+        goTo(baseUrl);
+        loginPage.isAt();
+        loginPage.typeEmailAddress("not a valid email");
+        loginPage.typePassword("any password");
+        loginPage.clickLogin();
+        loginPage.isAt();
+    }
+
+    @Test
+    public void itShouldBeNotPossibleToAccessSecurePagesWithoutLogin() {
+        goTo(baseUrl + "/pages/yourApps");
+        loginPage.isAt();
+    }
+
+    @Test
+    public void itShouldBePossibleToAccessRegistrationPagesWithCreateAccountButton() {
+        goTo(baseUrl);
+        loginPage.clickCreateAccount();
+        RegistrationPage.isAt();
+    }
+
+    @Test
+    public void itShouldNotBePossibleToSignUpWithAnInvalidEmail() {
+        goTo(baseUrl + "pages/accountRegistration");
+        RegistrationPage.isAt();
+
+        RegistrationPage.typeEmailAddress("not a valid email");
+        RegistrationPage.typeFirstname("FirstName");
+        RegistrationPage.typeLastname("LastName");
+        RegistrationPage.typePassword("any password");
+        RegistrationPage.typeConfirmPassword("any password");
+        RegistrationPage.clickSignUp();
+        RegistrationPage.isAt();
+
+    }
+
+    @Test
+    public void itShouldBePossibleToAccessYoutAppsPagesWithCreateAnAccount() {
+        goTo(baseUrl + "pages/accountRegistration");
+        RegistrationPage.isAt();
+
+        RegistrationPage.typeEmailAddress(newEmailId + "sara.kouch@gmail.ch");
+        RegistrationPage.typeFirstname("Sara");
+        RegistrationPage.typeLastname("Kouch");
+        RegistrationPage.typePassword("1234");
+        RegistrationPage.typeConfirmPassword("1234");
+        RegistrationPage.clickSignUp();
+        yourAppPage.isAt();
      // yourAppPage.clickLogout();
-        
-     }
-     @Test
-     public void itShouldBePossibleToSigninWithAnvalidEmail() {
-     goTo(baseUrl);
-     loginPage.isAt();
-     loginPage.typeEmailAddress("sara.kouch@gmail.ch");
-     loginPage.typePassword("1234");
-     loginPage.clickLogin();
-     yourAppPage.isAt();
+
+    }
+
+    @Test
+    public void itShouldBePossibleToSigninWithAnvalidEmail() {
+        goTo(baseUrl);
+        loginPage.isAt();
+        loginPage.typeEmailAddress("0sara.kouch@gmail.ch");
+        loginPage.typePassword("1234");
+        loginPage.clickLogin();
+        yourAppPage.isAt();
      //   yourAppPage.clickLogout();
-        
-     }
-    
-    
-     @Test
-     public void itShouldNotBePossibleToSignUpWithEmailThatIsNotUnique() {
-     goTo(baseUrl + "pages/accountRegistration");
-      
-     RegistrationPage.typeEmailAddress("raphael.racine@heig-vd.ch");
-     RegistrationPage.typeFirstname("firstname");
-     RegistrationPage.typeLastname("lastname");
-     RegistrationPage.typePassword("1234");
-     RegistrationPage.typeConfirmPassword("1234");
-     RegistrationPage.clickSignUp();
-     RegistrationPage.isAt();
-        
-     }
-    
-     
+
+    }
+
+    @Test
+    public void itShouldNotBePossibleToSignUpWithEmailThatIsNotUnique() {
+        goTo(baseUrl + "pages/accountRegistration");
+
+        RegistrationPage.typeEmailAddress("raphael.racine@heig-vd.ch");
+        RegistrationPage.typeFirstname("firstname");
+        RegistrationPage.typeLastname("lastname");
+        RegistrationPage.typePassword("1234");
+        RegistrationPage.typeConfirmPassword("1234");
+        RegistrationPage.clickSignUp();
+        RegistrationPage.isAt();
+
+    }
 
     @Test
     public void itShouldAllowTheUsersToRegisterNewApplication() {
         goTo(baseUrl);
 
-        loginPage.typeEmailAddress("sara.kouch@gmail.ch");
+        loginPage.typeEmailAddress(newEmailId + "sara.kouch@gmail.ch");
         loginPage.typePassword("1234");
         loginPage.clickLogin();
         yourAppPage.clickRegisterNewApp();
@@ -134,7 +138,7 @@ public class GamificationFluentTest extends FluentTest {
     public void itShouldBePossibleToRegisterNewAppWithSubmitNecessaryInformations() {
 
         goTo(baseUrl);
-        loginPage.typeEmailAddress("sara.kouch@gmail.ch");
+        loginPage.typeEmailAddress(newEmailId + "sara.kouch@gmail.ch");
         loginPage.typePassword("1234");
         loginPage.clickLogin();
         yourAppPage.clickRegisterNewApp();
@@ -144,14 +148,12 @@ public class GamificationFluentTest extends FluentTest {
         ApplicationRegistrationPage.clickRegister();
         yourAppPage.isAt();
     }
-    
+
     @Test
     public void itShouldNotBePossibleToRegisterAnApplicationWhitchAlreadyExists() {
 
-        goTo(baseUrl);
-        loginPage.typeEmailAddress("raphael.racine@heig-vd.ch");
-        loginPage.typePassword("toor");
-        loginPage.clickLogin();
+        simulateLogin();
+        
         yourAppPage.clickRegisterNewApp();
 
         ApplicationRegistrationPage.typeNameApp("Lego Creator");
@@ -160,9 +162,6 @@ public class GamificationFluentTest extends FluentTest {
         ApplicationRegistrationPage.isAt();
     }
 
-    
-    
-    
     @Override
     public WebDriver getDefaultDriver() {
         return new FirefoxDriver();
@@ -173,6 +172,11 @@ public class GamificationFluentTest extends FluentTest {
     @Override
     public String getDefaultBaseUrl() {
         return baseUrl;
+    }
+
+    @After
+    public void tearDown() {
+        newEmailId++;
     }
 
 }
