5082d9c546dd8a049cd2d452ed11cf65a58081b5
Raphaël Racine <raphaelracine@users.noreply.github.com>
2015-10-31 14:02:59 +0100

    Comments

--- src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/services/passwordvalidation/PasswordPoliticValidator.java
+++ src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/services/passwordvalidation/PasswordPoliticValidator.java
@@ -1,7 +1,18 @@
 package ch.heigvd.amt.gamification.services.passwordvalidation;
 
-public abstract class PasswordPoliticValidator {
+/**
+ * 
+ * This abstract class is used to check a politic on a password
+ * 
+ * @author Raphaël Racine
+ */
+public class PasswordPoliticValidator {
     
+    /**
+     * Check the password politic on the password in parameter
+     * @param password
+     * @throws BadPasswordException if the password doesnt respect politic
+     */
     public static void checkPassword(String password) throws BadPasswordException {
         if(password.length() < 8)
             throw new BadPasswordException("The password need to have minimum 8 characters of length");
--- src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/util/Pagination.java
+++ src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/util/Pagination.java
@@ -1,7 +1,18 @@
 package ch.heigvd.amt.gamification.util;
 
-public abstract class Pagination {
+/**
+ *
+ * @author Raphaël Racine
+ */
+public class Pagination {
     
+    /**
+     * Get the total numbers of the pages if we want nbItems per page et we have
+     * nbItems in total
+     * @param nbItems
+     * @param nbItemsPerPage
+     * @return the total numbers of the pages
+     */
     public static long computeTotalNbPages(long nbItems, long nbItemsPerPage) {
         return (long) Math.ceil((double) nbItems / nbItemsPerPage);
     }
