38d6c5b3614402ef7fcaf4abc816d5fa6f86bfa4
Raphaël Racine <raphaelracine@users.noreply.github.com>
2015-10-31 13:19:23 +0100

    Ajout d'un controlleur Ajax pour les applications et commentaires

--- src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/controllers/ListApplicationsAccountServlet.java
+++ src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/controllers/ListApplicationsAccountServlet.java
@@ -3,7 +3,6 @@ package ch.heigvd.amt.gamification.controllers;
 import ch.heigvd.amt.gamification.model.entities.Account;
 import ch.heigvd.amt.gamification.model.entities.Application;
 import ch.heigvd.amt.gamification.services.ApplicationsManagerLocal;
-import ch.heigvd.amt.gamification.services.dao.GamificationDomainEntityNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import javax.ejb.EJB;
@@ -21,10 +20,13 @@ public class ListApplicationsAccountServlet extends HttpServlet {
     @EJB
     private ApplicationsManagerLocal applicationsManager;
     
+    private final String TITLE = "Your Apps";
+    
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {       
         
-        request.setAttribute("title", "Your Apps");
+        request.setAttribute("title", TITLE);
         
         Account currentAccount = (Account) request.getSession().getAttribute("principal");
         
@@ -33,37 +35,6 @@ public class ListApplicationsAccountServlet extends HttpServlet {
         
         request.setAttribute("applications", applicationsOfCurrentAccount);        
         request.getRequestDispatcher("/WEB-INF/pages/applications_of_an_account.jsp").forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-        req.setAttribute("title", "Your Apps");
-        
-        // Disable or Enable application
-        String action = req.getParameter("action");
-        String idApplication = req.getParameter("idApplication");
-        
-        if(action != null && idApplication != null) {
-            
-            try {
-                Application app = applicationsManager.findById(Long.parseLong(idApplication));
-                        
-                        switch(action) {
-                            case "enableApplication":
-                                applicationsManager.enableApplication(app);
-                                break;
-                            case "disableApplication":
-                                applicationsManager.disableApplication(app);
-                                break;
-                        }
-                        
-            } catch (GamificationDomainEntityNotFoundException ex) {
-                resp.setStatus(500);
-            }
-        }
-        
-    }    
-    
+    }   
 
 }
