594dbb1b215ee2e2ae7e1eb0ea57f3af34790fdd
Raphaël Racine <raphaelracine@users.noreply.github.com>
2015-11-19 20:30:16 +0100

    Inserting event for an EndUser implementation

--- src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/services/EventsManager.java
+++ src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/services/EventsManager.java
@@ -2,6 +2,10 @@ package ch.heigvd.amt.gamification.services;
 
 import ch.heigvd.amt.gamification.model.EndUser;
 import ch.heigvd.amt.gamification.model.Event;
+import ch.heigvd.amt.gamification.services.dao.EndUserDAOLocal;
+import ch.heigvd.amt.gamification.services.dao.EventDAOLocal;
+import ch.heigvd.amt.gamification.services.dao.GamificationDomainEntityNotFoundException;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 /**
@@ -11,10 +15,22 @@ import javax.ejb.Stateless;
 @Stateless
 public class EventsManager implements EventsManagerLocal {
 
+    @EJB
+    private EventDAOLocal eventDAO;
+    
+    @EJB
+    private EndUserDAOLocal endUserDAO;
+    
     @Override
-    public void createEvent(Event event, EndUser endUser) {
-        // TODO:
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void createEvent(Event event, EndUser endUser) throws GamificationDomainEntityNotFoundException {
+        
+        // We need to attach the two entities
+        event.setEndUser(endUser);
+        endUser.getEvents().add(event);
+        
+        eventDAO.create(event);
+        endUserDAO.update(endUser);
+        
     }   
     
     
