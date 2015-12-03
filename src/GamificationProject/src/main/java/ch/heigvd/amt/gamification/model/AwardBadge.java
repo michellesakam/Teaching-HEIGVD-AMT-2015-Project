a1a94ececd301254f3ded0eb613027b3fdc92ca9
Alex <noums@gmail.com>
2015-12-03 17:50:23 +0100

    new

--- src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/model/AwardBadge.java
+++ src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/model/AwardBadge.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.heigvd.amt.gamification.model;
 
 import javax.persistence.DiscriminatorValue;
@@ -16,19 +11,19 @@ import javax.persistence.Entity;
 @DiscriminatorValue( "badge")
 public class AwardBadge extends Award{  
     // contient le lien vers l'image qui contient le badge
-    private String badge; 
+    private Badge badge; 
 
     /**
      * @return the badge
      */
-    public String getBadge() {
+    public Badge getBadge() {
         return badge;
     }
 
     /**
      * @param badge the badge to set
      */
-    public void setBadge(String badge) {
+    public void setBadge(Badge badge) {
         this.badge = badge;
     }
 }
---
+++ src/GamificationProject/src/main/java/ch/heigvd/amt/gamification/model/Badge.java
@@ -0,0 +1,9 @@
+package ch.heigvd.amt.gamification.model;
+
+/**
+ *
+ * @author parfait
+ */
+public class Badge {
+    private String badge;    
+}
