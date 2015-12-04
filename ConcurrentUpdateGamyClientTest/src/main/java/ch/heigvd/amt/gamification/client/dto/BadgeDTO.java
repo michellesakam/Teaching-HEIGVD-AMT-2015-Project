4d76bf5b06d695ae133d5cd2c21b5ba90c8bcd39
michellesakam <vanessasakam@gmail.com>
2015-12-04 11:04:20 +0100

    ajout du DTO badge dans le client test

---
+++ ConcurrentUpdateGamyClientTest/src/main/java/ch/heigvd/amt/gamification/client/dto/BadgeDTO.java
@@ -0,0 +1,28 @@
+package ch.heigvd.amt.gamification.client.dto;
+
+/**
+ *
+ * @author michelle meguep
+ */
+public class BadgeDTO{
+    
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }   
+    
+}
